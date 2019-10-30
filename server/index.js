fe06db930985ea601fc6fd2592b2ca3ec29a9716
Vezil <SzymonWojaczek@wp.pl>
2019-10-30 18:11:06 +0100

    adding users functions

--- server/index.js
+++ server/index.js
@@ -13,9 +13,15 @@ const io = socketio(server);
 io.on('connection',(socket) => {
     console.log('We have a new conection!');
 
+    socket.on('join', ({ name, room }, callback) =>{
+        console.log(name,room);
+
+
+    });
+
     socket.on('disconnect',() => {
         console.log('User had left!');
-    })
+    });
 });
 
 app.use(router);
