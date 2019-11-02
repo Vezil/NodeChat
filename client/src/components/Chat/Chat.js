92ec10a19eafaa58bd75603ccf569449f6a5a03b
Vezil <SzymonWojaczek@wp.pl>
2019-11-02 21:57:01 +0100

    Adding components: InfoBar, Input, Messages

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,9 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 import './Chat.css';
 
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
@@ -55,10 +58,9 @@ const Chat = ({ location }) => {
     return(
         <div className="outerContainer">
             <div className="container">
-                <input value={ message } 
-                onChange={(event) => setMessage(event.target.value)} 
-                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null }
-                />
+                <InfoBar room = { room }/>
+                <Input message = { message } setMessage = { setMessage } sendMessage = { sendMessage } />
+                <Messages/>
             </div>
         </div>
     )
