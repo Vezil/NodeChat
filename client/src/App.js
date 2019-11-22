269e1e9728671f4f1546bdfeae09d98c748dd167
Vezil <SzymonWojaczek@wp.pl>
2019-11-22 18:39:14 +0100

    add translations

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,53 @@
-import React from 'react';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { Switch, Route, withRouter } from "react-router-dom";
 
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
 
-const App = () => (
+import "./pageTransitions/slideTransition.scss";
 
-    <Router>
-        <Route path="/" exact component ={ Join } />
-        <Route path="/chat" exact component ={ Chat } />
-    </Router>
-);
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        prevDepth: this.getPathDepth(this.props.location)
+      };
+    }
 
-export default App;
+
+componentWillReceiveProps() {
+    this.setState({ prevDepth: this.getPathDepth(this.props.location)});
+}
+
+
+getPathDepth(location) {
+
+    let pathArr = location.pathname.split("/");
+    pathArr = pathArr.filter(n => n !== "");
+    return pathArr.length;
+}
+
+
+
+render ()  {
+    const { location } = this.props;
+    const currentKey = location.pathname.split("/")[1] || "/";
+    const timeout = { enter:800, exit: 400 };
+
+    return(
+    <TransitionGroup component="div" className="App">
+        <CSSTransition key={ currentKey } timeout={ timeout } classNames="pageSlider" mountOnEnter={false} unmountOnExit={true}>
+            <div className={this.getPathDepth(location) - this.state.prevDepth >= 0 ? "left" : "right"}>
+                <Switch location = { location }>
+                    <Route path="/" exact component ={ Join } />
+                    <Route path="/chat" exact component ={ Chat } />
+                </Switch>
+            </div>
+        </CSSTransition>
+    </TransitionGroup>
+    );
+  }
+}
+
+export default withRouter(App);
--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import closeIcon from '../../icons/closeIcon.png';
 import onlineIcon from '../../icons/onlineIcon.png';
+import { Link } from 'react-router-dom';
 
 import './InfoBar.css';
 
@@ -12,7 +13,8 @@ const InfoBar = ( { room } ) => (
             <h3><span className="room">Room :</span> { room }</h3>
         </div>
         <div className="rightInnerContainer">
-            <a href="/"><img src={ closeIcon } alt="close img"/></a>
+            {/* <a href="/"><img src={ closeIcon } alt="close img"/></a> */}
+            <Link to="/"><img src={ closeIcon } alt="close img"/></Link>
         </div>
     </div>
 
